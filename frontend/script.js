9382db88b66bc51f120c8b6145c3e74ef718c459
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2023-10-12 20:39:21 +0530

    commmit 1
    today 12/10/2023

--- frontend/script.js
+++ frontend/script.js
@@ -4,3 +4,18 @@ function togglemenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+
+
+const optionMenu = document.querySelector(".select-menu"),
+       selectBtn = optionMenu.querySelector(".select-btn"),
+       options = optionMenu.querySelectorAll(".option"),
+       sBtn_text = optionMenu.querySelector(".sBtn-text");
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+options.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+        optionMenu.classList.remove("active");
+    });
+});
