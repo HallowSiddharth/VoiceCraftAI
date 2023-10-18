25bc19ba486666a5fefdd082dfda982987d1671d
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2023-10-18 14:21:08 +0530

    texttypingeffect on 2 pages

--- frontend/message.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $file_name =  $_FILES['file']['name'];
-  $tmp_name = $_FILES['file']['tmp_name'];
-  $file_up_name = time().$file_name;
-  move_uploaded_file($tmp_name, "files/".$file_up_name);
-?>
